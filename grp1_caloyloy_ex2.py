bec9ce48d7ea9de2c59d3190e16152508a5334ea
salamanderO2 <caloyloyrodrigo_bscs@plmun.edu.ph>
2024-08-31 10:50:58 +0800

    Create grp1_caloyloy_ex2.py

---
+++ grp1_caloyloy_ex2.py
@@ -0,0 +1,36 @@
+print("Hello, I'm Caloyloy,Rodrigo A.! BSCS 3F")
+
+# Variables
+# a. Create two variables and assign number and name to it.
+age = 22
+student_name = "Caloyloy,Rodrigo A"
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+height, weight, shoe_size = 188, 120, 13
+print("Height:", height)
+print("Weight:", weight)
+print("Shoe Size:", shoe_size)
+
+# b. Then print both values
+print("Age:", age)
+print("Student Name:", student_name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+ronnie = "Ronnie"
+charles = "Charles"
+john_carlo = "John Carlo"  # Changed to use underscores
+alvinson = "Alvinson"
+
+# Print the values of the group member variables
+print("Group Member First Names:")
+print(ronnie)
+print(charles)
+print(john_carlo)
+print(alvinson)
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+status1 = status2 = status3 = "Active"
+print("Account Statuses:", status1, status2, status3)
